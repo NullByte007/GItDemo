dd9783f548106da584fdbc30724828301fad299b
ANIKET BHAGWATE <aniketbhagwate@gmail.com>
2021-08-09 20:24:36 +0530

    Add files via upload

---
+++ demo/src/test/java/com/Telstra/app/AppTest.java
@@ -0,0 +1,19 @@
+package com.Telstra.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{  
+
+     // Program for JUnit , checking for Errors !!!!!!!!!!
+     
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+}
